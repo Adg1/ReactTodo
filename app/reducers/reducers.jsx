943cf09d1b2149c57bb657ce0054cff3d1470e3d
Adg1 <amandeepgupta1@gmail.com>
2017-03-24 07:02:29 +0530

    Add todos reducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+const moment = require('moment');
+const uuid = require('uuid');
+
 export var searchTextReducer = (state = '',action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
@@ -7,12 +10,42 @@ export var searchTextReducer = (state = '',action) => {
   }
 };
 
-export var showCompletedReducer = (state = fasle, action) => {
+export var showCompletedReducer = (state = false, action) => {
   switch (action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
     default:
       return state;
 
+  }
+};
+
+export var todosReducer = (state =[], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ]
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          var nextCompleted = !todo.completed;
+
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined
+          };
+        }
+      });
+    default:
+      return state;
   }
 };
